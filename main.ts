8ba59e70fa0ae5f96a559754fb86cd42acaca6c3
shubhadip <shubhadip.maity@gmail.com>
2023-04-03 12:10:34 +0530

    refactor: removed github workflow

--- main.ts
+++ main.ts
@@ -1,8 +1,19 @@
 import { connect } from "https://deno.land/x/redis/mod.ts";
+import { Application } from "https://deno.land/x/oak/mod.ts";
+
 const redis = await connect({
   hostname: "127.0.0.1",
   port: 6379,
 });
-await redis.set("hoge", "fuga");
-const fuga = await redis.get("hoge");
-console.log("fuga", fuga);
+
+
+const app = new Application();
+
+app.use(async (ctx) => {
+  await redis.set("hoge", "fuga");
+  const fuga = await redis.get("hoge");
+
+  ctx.response.body = fuga + " Hello from Deno and AWS Lightsail!";
+});
+
+await app.listen({ port: 8000 });
